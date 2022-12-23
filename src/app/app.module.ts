cd13f2a7aa96fffcd139f31bfa16b594fb1732d9
tanaka <lemo.nade.engineer.room@gmail.com>
2022-12-23 22:28:02 +0900

    ホームの設定

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from '@/app/home/home.component';
-import { RouterModule } from "@angular/router"
+import { HomeComponent } from '@/app/mobile/home/home.component';
+import { RouterModule } from "@angular/router";
+import { EditButtonComponent } from './common/edit-button/edit-button.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    EditButtonComponent
   ],
   imports: [
     BrowserModule,
