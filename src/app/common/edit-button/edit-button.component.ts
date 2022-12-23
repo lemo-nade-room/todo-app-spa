4408d784f6367baca7e500b7d073f11a07f2b4cd
tanaka <lemo.nade.engineer.room@gmail.com>
2022-12-23 23:47:51 +0900

    編集ボタンを作成

---
+++ src/app/common/edit-button/edit-button.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+const EDIT = '編集';
+const END = '完了';
+
+@Component({
+  selector: 'app-edit-button',
+  templateUrl: './edit-button.component.html',
+  styleUrls: ['./edit-button.component.scss'],
+})
+export class EditButtonComponent implements OnInit {
+  constructor() {}
+
+  public ngOnInit(): void {}
+
+  @Input() public value!: boolean;
+  @Output() public valueChange = new EventEmitter<boolean>();
+  @Output() public click = new EventEmitter<undefined>();
+
+  public get display(): string {
+    return this.value ? END : EDIT;
+  }
+
+  public onClick() {
+    this.value = !this.value;
+    this.valueChange.emit(this.value);
+    this.click.emit();
+  }
+}
